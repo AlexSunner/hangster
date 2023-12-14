b020b098c50b7056c6d8d095973102b7b7bf49c5
Alexander Sunnerhage <alexsunnerhage@hotmail.com>
2023-12-14 16:19:41 +0000

    Add function that displays current state

--- run.py
+++ run.py
@@ -1,5 +1,21 @@
 import random
 
 def choose_word():
-    words = ["delicious", "christmas", "language", "artificial", "cranberry"]
+    """ 
+    Random words from a predefined list 
+    """
+    words = ["workspace", "develop", "programming", "python", "coding"]
     return random.choice(words)
+
+def display_word(word, guessed_letters):
+    """
+    Display the current state of the word, 
+    showing guessed letters and placeholders for unguessed letters
+    """
+    display = " "
+    for letter in word:
+        if letter in guessed_letters:
+            display += letter
+        else:
+            display += "_"
+        return display
