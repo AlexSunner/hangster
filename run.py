daa37b2c358cc0e202eb3d6c214ad8260868e0d9
Alexander Sunnerhage <alexsunnerhage@hotmail.com>
2023-12-14 17:02:20 +0000

    Create main function

--- run.py
+++ run.py
@@ -19,3 +19,30 @@ def display_word(word, guessed_letters):
         else:
             display += "_"
         return display
+
+def hangman():
+    """
+    Main function to run the game.
+    The function validates the user input,
+    Checks if the letter has already been guessed,
+    Add the guessed letter to the list,
+    Checks if the guessed letter is not in the word,
+    Checks if the players has more attempts, if not, game over.
+    """
+    max_attempts = 6
+    guessed_letters = []
+    word_to_guess = choose_word()
+
+    print("Welcome to Hangster!")
+
+    while True:
+        current_display = display_word(word_to_guess, guessed_letters)
+        print(f"Current word: {current_display}")
+        guess = input("Guess a letter: ").lower()
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("Please enter a single letter.")
+            continue
+
+        if guess in guessed_letters:
+            
